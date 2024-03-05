7eb8b1b55eb8d0023d4f99fbeae80747b4b27d34
ArrietA <andresarrieta2323@gmail.com>
2024-03-04 18:22:27 -0600

    Cambie el type del input de telephone a tel

--- src/components/Service/Book.jsx
+++ src/components/Service/Book.jsx
@@ -83,7 +83,7 @@ export function Book({firstCard = ""}) {
           Phone number
         </label>
         <input
-          type="text"
+          type="tel"
           id="phone"
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
           placeholder="+506 4354 6865"
@@ -178,7 +178,7 @@ export function Book({firstCard = ""}) {
           Phone number
         </label>
         <input
-          type="text"
+          type="tel"
           id="phone"
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
           placeholder="+506 4354 6865"
