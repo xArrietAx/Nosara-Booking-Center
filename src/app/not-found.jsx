4f5f1d0679911f4bc9c166ea680f239485dd9d9a
ArrietA <andresarrieta2323@gmail.com>
2024-06-18 09:31:45 -0600

    Habia comentado la pagina de error 404 para una cosa, pero ya la descomente

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,54 +2,29 @@ import Image from "next/image";
 import Link from "next/link";
 
 function Page404() {
-  return <main>
-    {/* <section className="container relative pt-5 pb-16 lg:pb-20 lg:pt-5">
-      <div className="text-center max-w-2xl mx-auto space-y-2">
-        <Image src={"/404.webp"} alt="not-found" width={1500} height={1000} priority />
-        <span className="block text-sm text-gray-800 sm:text-base tracking-wider font-medium">
-         {"THE PAGE YOU WERE LOOKING FOR DOESN'T EXIST"}
-        </span>
-        <div className="pt-8">
-          <Link href="/" className="custom-btn">Return Home Page</Link>
+  return (
+    <main>
+      <section className="container relative pt-5 pb-16 lg:pb-20 lg:pt-5">
+        <div className="text-center max-w-2xl mx-auto space-y-2">
+          <Image
+            src={"/404.webp"}
+            alt="not-found"
+            width={1500}
+            height={1000}
+            priority
+          />
+          <span className="block text-sm text-gray-800 sm:text-base tracking-wider font-medium">
+            {"THE PAGE YOU WERE LOOKING FOR DOESN'T EXIST"}
+          </span>
+          <div className="pt-8">
+            <Link href="/" className="custom-btn">
+              Return Home Page
+            </Link>
+          </div>
         </div>
-      </div>
-    </section> */}
-
-
-{/* <p className="text-gray-6000">
-            Experience the ultimate family getaway at Casa Cocobolo, a charming
-            villa. This 3-bedroom, 2-bathroom retreat features a private pool,
-            spacious garden, and a delightful BBQ deck area. Perched above the
-            lush jungle, enjoy in the breathtaking beautiful landscape.
-            <br />
-            Immerse yourself in relaxation and the beauty of Nosara's
-            picturesque area during your stay.
-      </p>
-<div>
-      <span className="font-semibold">The space</span>
-      <p className="text-gray-6000">
-        Our houses is a three bedroom which consists of a master that has its
-        own wardrobe closet and bathroom. The other two bedrooms share a
-        bathroom on the other side of the house and is perfect for kids or other
-        friends staying with you.
-        <br />
-        <br />
-        We have a dipping pool off the living and kitchen area which acts as a
-        great space to all be together.
-      </p>
-</div>
-
-<div>
-    <span className="font-semibold">Guest access</span>
-     <p className="text-gray-6000">
-      You have the use of the entire home, deck and land. There is guest parking on site behind two lockable entrance gates.
-     </p>
-</div> */}
-
-
-
-  </main>
-
+      </section>
+    </main>
+  );
 }
 
 export default Page404;
